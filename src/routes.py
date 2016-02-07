95a639492d49f9d3a7ecfd41718061f9f9c6fcbf
Aishwarya Sharma <me@weirdoaish.com>
2016-02-07 18:01:11 -0500

    Updated routes

--- src/routes.py
+++ src/routes.py
@@ -1,21 +1,48 @@
-from bottle import route, static_file, error
+from bottle import route, static_file, error, post, request
+from jinja2 import Environment, FileSystemLoader
+from database import word_matcher
 
 __author__ = "Aishwarya Sharma"
 
 
+# Specifies that our templates are located in the "templates" folder inside
+# the "src" python package
+template_env = Environment(
+    loader=FileSystemLoader('templates', followlinks=True))
+
+
 # Web App Entry point. Returns index.html
 @route('/')
+@route('/index')
+@route('/home')
 def index():
-    return static_file("index.html", root="static")
+    return template_env.get_template("home.html").render()
 
 
-# Route to serve static files. Uses regex to resolve filename.
-@route('/:filename#.*#')
-def static(filename):
-    return static_file(filename, root="static")
+# Searching for words matching given criteria.
+@post('/search')
+def search():
+    try:
+        char_list = str(request.forms.get('charSet')).strip()
+        word_length = int(str(request.forms.get('wordLength')).strip())
+    except ValueError:
+        print("Had a value error")
+        return template_env.get_template("search_error.html").render()
+
+    if char_list == "":
+        print("Char list is empty")
+        return template_env.get_template("search_error.html").render()
+
+    words = word_matcher.get_matching_words(char_list, word_length)
+
+    if words is None or len(words) == 0:
+        print("No Matching words found")
+        return template_env.get_template("search_error.html").render()
+
+    return template_env.get_template("search_results.html").render(words=words)
 
 
 # Route for 404 errors.
-@error
-def error():
-    return static_file("404.html", root="static")
+@error(404)
+def error404(error):
+    return template_env.get_template("404.html").render()
