3e2a0421fd3da10b66afa1418e26746a6d469505
Aishwarya Sharma <me@weirdoaish.com>
2016-02-07 20:42:07 -0500

    Updated to run on port 80 for deployment

--- src/run.py
+++ src/run.py
@@ -6,4 +6,4 @@ __author__ = "Aishwarya Sharma"
 
 if __name__ == "__main__":
     # Starting Server
-    run(server="cherrypy", host="localhost", port=8080, debug=True)
+    run(server="cherrypy", host="localhost", port=80, debug=True)
