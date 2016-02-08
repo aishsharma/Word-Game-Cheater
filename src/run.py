8e3c193394e62a7d27a7066bafb5a8eb7df85d45
Aishwarya Sharma <me@weirdoaish.com>
2016-02-07 20:10:55 -0500

    Using cherrypy as web server

--- src/run.py
+++ src/run.py
@@ -1,8 +1,9 @@
 from bottle import run
+import cherrypy
 from routes import *
 
 __author__ = "Aishwarya Sharma"
 
 if __name__ == "__main__":
     # Starting Server
-    run(host="localhost", port=8080, debug=True)
+    run(server="cherrypy", host="localhost", port=8080, debug=True)
